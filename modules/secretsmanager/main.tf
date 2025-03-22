0f43f445f66053f65309128cd2387d11db2915eb
aarontravass <43901677+aarontravass@users.noreply.github.com>
2025-03-21 21:28:36 -0400

    Dev (#3)
    * defined ambee data key
    
    * ci: set up actions
    
    * ci: changed working dir
    
    * ci: changed working dir
    
    * style: fmt the code
    
    * ci: bug fixes
    
    * added plan
    
    * added plan
    
    * added plan
    
    * added apply workflow
    
    * added apply workflow
    
    * ci: tf should work
    
    * ci: fixed ci issues
    
    * fix: fixed merge conflicts

--- modules/secretsmanager/main.tf
+++ modules/secretsmanager/main.tf
@@ -1,3 +1,7 @@
 resource "aws_secretsmanager_secret" "EXTERNAL_SVC_API_KEYS" {
   name = "EXTERNAL_SVC_API_KEYS"
 }
+
+resource "aws_secretsmanager_secret" "EXAMPLE_SECRET" {
+  name = "EXAMPLE_SECRET"
+}
